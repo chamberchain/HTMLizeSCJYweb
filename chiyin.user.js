19da48c4dee1e02b0e459d05f5aa8b10f7fd7de3
chamberchain <chamberchain@users.noreply.github.com>
2016-10-16 13:44:29 +0800

    Update chiyin.user.js

--- chiyin.user.js
+++ chiyin.user.js
@@ -7,7 +7,26 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-unsafeWindow.ShowTable = unsafeWindow.Getpath = function (event) {
+document.addEventListener('DOMContentLoaded', function (e) {
+  var dlinks = document.querySelectorAll('div.scon_r ul:last-child li span:nth-child(1) a');
+  if (!!dlinks) {
+    for (var i = 0; i < dlinks.length; i++) {
+      var blink = document.createElement('a');
+      blink.href = '###';
+      blink.setAttribute('data-id', dlinks[i].getAttribute('data-id'));
+      blink.setAttribute('data-type', dlinks[i].getAttribute('data-type'));
+      blink.setAttribute('onclick', 'ShowTable(this)');
+      blink.title = '下载';
+      var dimg = document.createElement('img');
+      dimg.setAttribute('onmouseover', 'this.src=img4.src');
+      dimg.setAttribute('onmouseout', 'this.src=img3.src');
+      dimg.src = 'http://www.chiyin168.com/Image/yytb_02.png';
+      blink.appendChild(dimg);
+      dlinks[i].parentNode.appendChild(blink);
+    }
+  }
+}, false);
+unsafeWindow.ShowTable = function (event) {
   var id = $(event).attr('data-id') || $(this).attr('data-id');
   var type = $(event).attr('data-type') || $(this).attr('data-type');
   $.post('/home/TrialSongs', {
@@ -15,21 +34,12 @@ unsafeWindow.ShowTable = unsafeWindow.Getpath = function (event) {
     type: type
   }, function (data) {
     if (data.msg === true) {
-      var player=document.querySelector("audio#custom-player");
-      if (player){
-        document.body.removeChild(player);
-      }
-      var audio=document.createElement("audio");
-      audio.id="custom-player";
-      audio.src=data.url;
-      document.body.appendChild(audio);
-      audio.play();
       var alink = document.createElement('a');
       alink.href = data.url;
       alink.download = data.name;
       document.body.appendChild(alink);
       alink.click();
-      document.body.removeChild(alink); 
+      document.body.removeChild(alink);
     } else {
       alert(data.text);
     }
