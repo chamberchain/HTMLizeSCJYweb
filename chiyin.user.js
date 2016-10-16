325a853537e5543d925a3348285dcbd025eee528
chamberchain <chamberchain@users.noreply.github.com>
2016-10-16 09:10:36 +0800

    Update chiyin.user.js

--- chiyin.user.js
+++ chiyin.user.js
@@ -3,12 +3,11 @@
 // @namespace   cc
 // @description shit
 // @include     http://www.chiyin168.com/*
-// @exclude     http://www.chiyin168.com/Mall/*
 // @exclude     http://www.chiyin168.com/ShiPin/*
 // @version     1
 // @grant       none
 // ==/UserScript==
-unsafeWindow.ShowTable = function (event) {
+unsafeWindow.ShowTable = unsafeWindow.Getpath = function (event) {
   var id = $(event).attr('data-id') || $(this).attr('data-id');
   var type = $(event).attr('data-type') || $(this).attr('data-type');
   $.post('/home/TrialSongs', {
