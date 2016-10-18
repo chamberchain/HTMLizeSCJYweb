33a362ca7f7c8530746177afe3eaef37c3d7d966
chamberchain <chamberchain@users.noreply.github.com>
2016-10-18 15:02:28 +0800

    Update chiyin.user.js

--- chiyin.user.js
+++ chiyin.user.js
@@ -9,24 +9,6 @@
 // @updateURL	https://github.com/chamberchain/HTMLizeSCJYweb/raw/master/chiyin.user.js
 // @grant       none
 // ==/UserScript==
-$(document).ready(function () {
-  var dlinks = $('div.scon_r ul:last-child li span:nth-child(1) a');
-  dlinks = (dlinks.length === 0) ? $('div.yuanc1 div.yc_con a.shiting.trial')  : dlinks;
-  if (!!dlinks) {
-    for (var i = 0; i < dlinks.length; i++) {
-      var blink = document.createElement('a');
-      blink.setAttribute('class', 'doload');
-      blink.href = '###';
-      blink.setAttribute('data-id', dlinks[i].getAttribute('data-id'));
-      blink.setAttribute('data-type', dlinks[i].getAttribute('data-type'));
-      blink.title = '免费下载';
-      var dimg = document.createElement('img');
-      dimg.src = 'http://www.chiyin168.com/Image/yytb_02.png';
-      blink.appendChild(dimg);
-      dlinks[i].parentNode.appendChild(blink);
-    }
-  }
-});
 var st = function (event) {
   var id = $(event).attr('data-id') || $(this).attr('data-id');
   var type = $(event).attr('data-type') || $(this).attr('data-type');
@@ -47,5 +29,24 @@ var st = function (event) {
     }
   }, 'json');
 };
- $('a[onclick="ShowTable(this)"]').removeAttr('onclick').on('click', st);
- $('.doload').off('click', ShowTable).on('click', st);
+$(document).ready(function () {
+  var dlinks = $('div.scon_r ul:last-child li span:nth-child(1) a');
+  dlinks = (dlinks.length === 0) ? $('div.yuanc1 div.yc_con a.shiting.trial')  : dlinks;
+  if (!!dlinks) {
+    for (var i = 0; i < dlinks.length; i++) {
+      var blink = document.createElement('a');
+      blink.setAttribute('class', 'doload');
+      blink.href = '###';
+      blink.setAttribute('data-id', dlinks[i].getAttribute('data-id'));
+      blink.setAttribute('data-type', dlinks[i].getAttribute('data-type'));
+      blink.title = '免费下载';
+      var dimg = document.createElement('img');
+      dimg.src = 'http://www.chiyin168.com/Image/yytb_02.png';
+      blink.appendChild(dimg);
+      dlinks[i].parentNode.appendChild(blink);
+    }
+  }
+});
+$('a[onclick="ShowTable(this)"]').removeAttr('onclick').on('click', st);
+$('.doload').off('click', ShowTable).on('click', st);
+ 
