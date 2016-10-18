c56e6b60253c8ca550448e7f6282f970b7f3cf51
chamberchain <chamberchain@users.noreply.github.com>
2016-10-18 08:49:12 +0800

    Update chiyin.user.js

--- chiyin.user.js
+++ chiyin.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        chiyin168
-// @version     1.1.1.3
+// @version     1.1.1.5
 // @namespace   cc
 // @description shit
 // @include     http://www.chiyin168.com/*
@@ -9,30 +9,27 @@
 // @updateURL	https://github.com/chamberchain/HTMLizeSCJYweb/raw/master/chiyin.user.js
 // @grant       none
 // ==/UserScript==
-$(document).ready(function(){
-var dlinks=$("div.scon_r ul:last-child li span:nth-child(1) a");
-dlinks=(dlinks.length===0)?$("div.yuanc1 div.yc_con a.shiting.trial"):dlinks;
-if (!!dlinks) {
+$(document).ready(function () {
+  var dlinks = $('div.scon_r ul:last-child li span:nth-child(1) a');
+  dlinks = (dlinks.length === 0) ? $('div.yuanc1 div.yc_con a.shiting.trial')  : dlinks;
+  if (!!dlinks) {
     for (var i = 0; i < dlinks.length; i++) {
       var blink = document.createElement('a');
+      blink.setAttribute('class', 'doload');
       blink.href = '###';
       blink.setAttribute('data-id', dlinks[i].getAttribute('data-id'));
       blink.setAttribute('data-type', dlinks[i].getAttribute('data-type'));
-      blink.setAttribute('onclick', 'ShowTable(this)');
       blink.title = '免费下载';
       var dimg = document.createElement('img');
-      //dimg.setAttribute('onmouseover', 'this.src=img4.src');
-      //dimg.setAttribute('onmouseout', 'this.src=img3.src');
       dimg.src = 'http://www.chiyin168.com/Image/yytb_02.png';
       blink.appendChild(dimg);
       dlinks[i].parentNode.appendChild(blink);
     }
-}
+  }
+  $('a[onclick="ShowTable(this)"]').removeAttr('onclick').on('click', st);
+  $('.doload').off('click', ShowTable).on('click', st);
 });
-if (typeof(unsafeWindow)=="undefined"){
-   var unsafeWindow=window;
-}
-unsafeWindow.ShowTable = function (event) {
+var st = function (event) {
   var id = $(event).attr('data-id') || $(this).attr('data-id');
   var type = $(event).attr('data-type') || $(this).attr('data-type');
   $.post('/home/TrialSongs', {
@@ -42,7 +39,7 @@ unsafeWindow.ShowTable = function (event) {
     if (data.msg === true) {
       var alink = document.createElement('a');
       alink.href = data.url;
-      alink.download = data.name.replace(/\./g," ");
+      alink.download = data.name.replace(/\./g, ' ');
       document.body.appendChild(alink);
       alink.click();
       document.body.removeChild(alink);
